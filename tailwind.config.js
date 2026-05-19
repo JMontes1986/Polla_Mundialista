09cf0dac197f0597b74bb25160477d9fe29f96c2
JMontes <damon.1986@icloud.com>
2026-05-19 14:49:23 -0500

    Revisar problemas de visualización en la pantalla principal

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,5 @@
-// tailwind.config.ts
-import type { Config } from 'tailwindcss'
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -23,5 +21,3 @@ const config: Config = {
   },
   plugins: [],
 }
-
-export default config
